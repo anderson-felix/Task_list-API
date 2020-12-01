42f935732ac5ae95367c52341d5e2a2d3aab968d
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-11-30 23:25:28 -0300

    add methods put, get and delete tasks

--- tasklist/src/controllers/TaskController.ts
+++ tasklist/src/controllers/TaskController.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getConnection, getRepository } from 'typeorm';
 import * as Yup from 'yup';
 
 import Task from '../models/task';
 import User from './../models/user';
 
 class TaskController {
+  async index(req: Request, res: Response) {
+    const taskRepo = getRepository(Task);
+    const userRepo = getRepository(User);
+
+    const taskUser = await userRepo.findOne({
+      where: { id: req.userId },
+    });
+
+    const tasks = await taskRepo.find({
+      where: { userId: taskUser },
+    });
+
+    return res.json(tasks);
+  }
   async store(req: Request, res: Response) {
     const schema = Yup.object().shape({
       task: Yup.string().required('Empty field.'),
@@ -18,8 +32,8 @@ class TaskController {
       return res.status(401).json({ error: 'Authentication error' });
     }
 
-    const userRepository = getRepository(User);
-    const user = await userRepository.findOne({
+    const userRepo = getRepository(User);
+    const user = await userRepo.findOne({
       where: { id: userId },
     });
 
@@ -29,10 +43,10 @@ class TaskController {
     };
     await schema.validate(data, { abortEarly: false });
 
-    const taskRepository = getRepository(Task);
+    const taskRepo = getRepository(Task);
 
-    const createTask = taskRepository.create(data);
-    await taskRepository.save(createTask);
+    const createTask = taskRepo.create(data);
+    await taskRepo.save(createTask);
 
     return res.json({
       id: createTask.id,
@@ -42,6 +56,49 @@ class TaskController {
       task: createTask.task,
       check: createTask.check,
     });
+  }
+  async update(req: Request, res: Response) {
+    const { task_id } = req.params;
+    const taskRepo = getRepository(Task);
+
+    const taskId = parseInt(task_id);
+
+    try {
+      await getConnection()
+        .createQueryBuilder()
+        .update(Task)
+        .set({
+          check: req.body.check,
+        })
+        .where('id = :task_id', { task_id })
+        .execute();
+    } catch (e) {
+      return res.json({ error: 'Task not exists', e });
+    }
+    const task = await taskRepo.findOneOrFail({
+      where: { id: taskId },
+    });
+    return res.json(task);
+  }
+  async delete(req: Request, res: Response) {
+    const { task_id } = req.params;
+    const taskRepo = getRepository(Task);
+    const taskId = parseInt(task_id);
+
+    try {
+      const task = await taskRepo.findOne({
+        where: { id: taskId },
+      });
+
+      if (task.userId !== req.userId) {
+        return res.status(401).json({ error: 'Not authorized' });
+      }
+    } catch (e) {
+      return res.json({ error: 'Task not exists', e });
+    }
+
+    await taskRepo.delete({ id: taskId });
+    return res.status(200).json({ message: 'Deleted' });
   }
 }
 
