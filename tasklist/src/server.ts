7e159d4c07ede1180c374279fcedee35742ce2b9
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-12-04 22:59:11 -0300

    Update server.ts

--- tasklist/src/server.ts
+++ tasklist/src/server.ts
@@ -1,19 +1,7 @@
 import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-
-import './database/connection';
-import routes from './routes';
-import errorHandler from './errors/handler';
 
 const server = express();
 
-server.use(cors()); // TODOS SERVER.USE VÃO FICAR NO ARQUIVO APP.TS EM MIDDLEWARES
-
-server.use(express.json());
-
-server.use(routes);
-
-server.use(errorHandler);
-
 server.listen(4000);
+
+export default server;
